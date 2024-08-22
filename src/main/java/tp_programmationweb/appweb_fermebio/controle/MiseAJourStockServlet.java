7df2ae07e214058240d7af7e199d2bd4a62a1814
Fatma_Zohra_SOUMATIA <6283978@bdeb.qc.ca>
2024-08-22 06:02:39 -0400

    ....

---
+++ src/main/java/tp_programmationweb/appweb_fermebio/controle/MiseAJourStockServlet.java
@@ -0,0 +1,34 @@
+package tp_programmationweb.appweb_fermebio.controle;
+
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+
+@WebServlet(name = "MiseAJourStockServlet", value = "/MiseAJourStockServlet")
+public class  MiseAJourStockServlet  extends HttpServlet {
+
+
+    public void init() {
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    }
+
+
+    public void destroy() {
+    }
+
+}
+
+
+
